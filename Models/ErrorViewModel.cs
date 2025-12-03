05a786f5564d490017e0723d16c8d1f3027b2538
tranlongvu2004 <tlv04102004@gmail.com>
2025-12-03 19:43:58 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PorjectManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
