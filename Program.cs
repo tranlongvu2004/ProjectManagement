b572147b4af002c33ba44989be1c2e22665d7802
tranlongvu2004 <tlv04102004@gmail.com>
2025-12-04 22:28:10 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
+using PorjectManagement.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSqlServer<LabProjectManagementContext>(builder.Configuration.GetConnectionString("MyCnn"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
