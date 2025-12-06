cc35022319f96592417d2542c80492369cabf24a
DucNghiem <147522506+DucNghiem@users.noreply.github.com>
2025-12-06 21:38:25 +0700

    add backlog controller

---
+++ Controllers/BacklogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PorjectManagement.Controllers
+{
+    public class BacklogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
