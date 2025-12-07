fd062a718105a2f99d78d4d7414c25952b3cc97c
DucNghiem <147522506+DucNghiem@users.noreply.github.com>
2025-12-07 22:53:56 +0700

    update backlog controller

---
+++ Controllers/BacklogController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PorjectManagement.Models;
+
+namespace PorjectManagement.Controllers
+{
+    public class BacklogController : Controller
+    {
+        private LabProjectManagementContext _context;
+        public BacklogController(LabProjectManagementContext context)
+        {
+            _context = context;
+        }
+        public IActionResult BacklogUI()
+        {
+            var tasks = _context.Tasks.Include(t => t.CreatedByNavigation).ToList();
+            return View(tasks);
+        }
+    }
+}
