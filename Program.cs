6ebeb1a3b563cfa3bb4f7482b53f02096942ba77
DucNghiem <147522506+DucNghiem@users.noreply.github.com>
2025-12-07 15:28:33 +0700

    update backlog

--- Program.cs
+++ Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=User}/{action=Login}/{id?}");
+    pattern: "{controller=Backlog}/{action=BacklogUI}/{id?}");
 
 app.Run();
