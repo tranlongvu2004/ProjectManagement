112d433d4334a267cc846d9b695880b0aa06ac8d
jngon9 <nghianhhe182026@fpt.edu.vn>
2025-12-08 11:02:18 +0700

    update workspace v4

--- Controllers/WorkspaceController.cs
+++ Controllers/WorkspaceController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using PorjectManagement.Models;
 using PorjectManagement.Service.Interface;
 
 namespace PorjectManagement.Controllers
@@ -8,12 +6,10 @@ namespace PorjectManagement.Controllers
     public class WorkspaceController : BaseController
     {
         private readonly IProjectServices _projectServices;
-        private readonly LabProjectManagementContext _context;
 
-        public WorkspaceController(IProjectServices projectServices, LabProjectManagementContext context)
+        public WorkspaceController(IProjectServices projectServices)
         {
             _projectServices = projectServices;
-            _context = context;
         }
 
         // GET: /Workspace/Details/5
@@ -38,14 +34,6 @@ namespace PorjectManagement.Controllers
             int currentUserId = HttpContext.Session.GetInt32("UserId") ?? 0;
             var projects = await _projectServices.GetProjectsOfUserAsync(currentUserId);
             ViewBag.Projects = projects;
-
-            // ✅ Lấy tasks cho Backlog
-            var tasks = await _context.Tasks
-                .Include(t => t.CreatedByNavigation)
-                .Where(t => t.ProjectId == id)
-                .ToListAsync();
-            ViewBag.BacklogTasks = tasks;
-            ViewBag.ProjectId = id;
 
             return View(model);
         }
