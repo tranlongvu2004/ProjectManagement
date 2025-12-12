728824256321c842b89fdef0296e931a6b528903
tranlongvu2004 <tlv04102004@gmail.com>
2025-12-12 13:37:58 +0700

    testing

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -92,7 +92,12 @@ namespace PorjectManagement.Controllers
                 ViewBag.Error = "Email không đúng định dạng.";
                 return View();
             }
-
+            var existUser = _userService.GetUser(email);
+            if (existUser != null)
+            {
+                ViewBag.Error = "Email đã tồn tại.";
+                return View();
+            }
             if (password != confirmpassword)
             {
                 ViewBag.Error = "Mật khẩu xác nhận không khớp.";
@@ -105,17 +110,11 @@ namespace PorjectManagement.Controllers
                 ViewBag.Error = "Vui lòng nhập đầy đủ thông tin.";
                 return View();
             }
-            if (password.Length <= 4 && confirmpassword.Length <= 4)
+            if (password.Length <= 4 || confirmpassword.Length <= 4)
             {
                 ViewBag.Error = "Mật khẩu phải có ít nhất 4 ký tự.";
                 return View();
-            }
-            var existUser = _userService.GetUser(email);
-            if (existUser != null)
-            {
-                ViewBag.Error = "Email đã tồn tại.";
-                return View();
-            }
+            }            
             var newUser = new User
             {
                 FullName = fullName,
@@ -171,14 +170,13 @@ namespace PorjectManagement.Controllers
                 ViewBag.Email = email;
                 return View();
             }
-
             if (newpassword != confirmpassword)
             {
                 ViewBag.Error = "Mật khẩu xác nhận không khớp.";
                 ViewBag.Email = email;
                 return View();
             }
-            if (newpassword.Length <= 4 && confirmpassword.Length <= 4)
+            if (newpassword.Length <= 4 || confirmpassword.Length <= 4)
             {
                 ViewBag.Error = "Mật khẩu phải có ít nhất 4 ký tự.";
                 return View();
@@ -188,7 +186,7 @@ namespace PorjectManagement.Controllers
             {
                 ViewBag.Error = "Không tìm thấy tài khoản.";
                 return View();
-            }
+            }                                
             user.PasswordHash = newpassword;
             _userService.UpdateUser(user);
 
