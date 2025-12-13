aff93f3703e5afe664ed5bbe7269acbc4cd632b0
tranlongvu2004 <tlv04102004@gmail.com>
2025-12-14 03:43:42 +0700

    .

---
+++ ViewModels/TaskAssignViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace PorjectManagement.ViewModels
+{
+    public class TaskAssignViewModel
+    {
+        public int TaskId { get; set; }
+
+        public int SelectedUserId { get; set; }
+        public int ProjectId { get; set; }
+        [ValidateNever]
+        public List<UserListItemVM> Users { get; set; }
+    }
+}
