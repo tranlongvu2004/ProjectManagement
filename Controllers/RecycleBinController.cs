25c22462f63131c61703ac26a5da7fdd29331c5c
DucNghiem <147522506+DucNghiem@users.noreply.github.com>
2025-12-13 19:13:55 +0700

    add recycle bin view and controller

---
+++ Controllers/RecycleBinController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PorjectManagement.Controllers
+{
+    public class RecycleBinController : Controller
+    {
+        public IActionResult RecycleBin()
+        {
+            return View();
+        }
+    }
+}
